0db135644e66d04e576a483e93c45f3bc8358b47
jdagdelen <jdagdelen@lbl.gov>
2020-11-04 15:09:24 -0800

    Switched to SGD instead of Adam and using linear decay learning rate. Also added separate LR for embeddings and HR model.

--- anymat2vec/hidden_rep/trainer.py
+++ anymat2vec/hidden_rep/trainer.py
@@ -2,16 +2,17 @@
 Parts of this code were adapted from https://github.com/Andras7/word2vec-pytorch/blob/master/word2vec/model.py
 
 """
+import os
+
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
+
 from anymat2vec import MODELS_DIR
 from anymat2vec.hidden_rep.data import DataReader, HiddenRepDataset
 from anymat2vec.hidden_rep.model import HiddenRepModel
-import os
-from torch.utils.tensorboard import SummaryWriter
-import numpy as np
 
 
 class HiddenRepTrainer:
@@ -20,7 +21,8 @@ class HiddenRepTrainer:
     """
 
     def __init__(self, input_file, save_directory_name="hr_save", emb_dimension=200, hidden_size=20, batch_size=32,
-                 window_size=8, n_epochs=30, initial_lr=0.001, min_count=10, use_vanilla_word2vec=False):
+                 window_size=8, n_epochs=30, initial_lr=10, min_count=10, use_vanilla_word2vec=False,
+                 hidden_lr=None, min_lr=1E-6):
 
         _, file_extension = os.path.splitext(input_file)
 
@@ -44,7 +46,8 @@ class HiddenRepTrainer:
         self.emb_dimension = emb_dimension
         self.batch_size = batch_size
         self.n_epochs = n_epochs
-        self.initial_lr = initial_lr
+        self.embed_lr = initial_lr
+        self.min_lr = min_lr
         self.hidden_size = hidden_size
         self.stoichiometries = torch.cat((torch.zeros((self.data.num_regular_words,
                                                        self.data.stoichiometries.size()[1]),
@@ -60,6 +63,26 @@ class HiddenRepTrainer:
         self.device = torch.device("cuda" if self.use_cuda else "cpu")
         if self.use_cuda:
             self.hidden_rep_model.cuda()
+        if hidden_lr:
+            self.hidden_lr = hidden_lr
+        else:
+            self.hidden_lr = initial_lr
+
+        self.optimizer = optim.SGD([
+            {'params': self.hidden_rep_model.u_embeddings.parameters(), "type": "embeddings"},
+            {'params': self.hidden_rep_model.v_embeddings.parameters(), "type": "embeddings"},
+            {'params': self.hidden_rep_model.shared_generator.parameters(), 'lr': self.hidden_lr, "type": "generator"},
+            {'params': self.hidden_rep_model.tmeg.parameters(), 'lr': self.hidden_lr, "type": "generator"},
+            {'params': self.hidden_rep_model.cmeg.parameters(), 'lr': self.hidden_lr, "type": "generator"}
+        ], self.embed_lr)
+
+    def get_next_lr(self, start_lr, end_lr, epoch_progress, epoch):
+        """Get the correct learning rate for the next iteration.
+        """
+        progress = (epoch + epoch_progress) / self.n_epochs
+        next_lr = start_lr - (start_lr - end_lr) * progress
+        next_lr = max(end_lr, next_lr)
+        return next_lr
 
     def train(self):
 
@@ -67,30 +90,40 @@ class HiddenRepTrainer:
 
         # Send stoichiometry tensor to GPU
         self.stoichiometries = self.stoichiometries.to(self.device)
-        optimizer = optim.Adam(self.hidden_rep_model.parameters(), lr=self.initial_lr)
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(self.dataloader) * self.n_epochs)
 
         for epoch in range(self.n_epochs):
+            epoch_size = len(self.dataloader)
             print("\n\n\nEpoch: " + str(epoch + 1))
             running_loss = 0.0
             for i, sample_batched in enumerate(tqdm(self.dataloader)):
+                epoch_progress = i / epoch_size
+                for param_group in self.optimizer.param_groups:
+                    if param_group["type"] == "generator":
+                        param_group['lr'] = self.get_next_lr(self.hidden_lr,
+                                                             self.min_lr,
+                                                             epoch_progress,
+                                                             epoch)
 
+                    elif param_group["type"] == "embeddings":
+                        param_group['lr'] = self.get_next_lr(self.embed_lr,
+                                                             self.min_lr,
+                                                             epoch_progress,
+                                                             epoch)
                 if len(sample_batched[0]) > 1:
 
                     pos_u = sample_batched[0].to(self.device)
                     pos_v = sample_batched[1].to(self.device)
                     neg_v = sample_batched[2].to(self.device)
 
-                    scheduler.step()
-                    optimizer.zero_grad()
+                    self.optimizer.zero_grad()
                     loss = self.hidden_rep_model.forward(pos_u, pos_v, neg_v)
                     loss.backward()
-                    optimizer.step()
+                    self.optimizer.step()
 
                     running_loss = running_loss * 0.9 + loss.item() * 0.1
                     if i > 0 and i % 100 == 0:
                         print(" Loss: " + str(running_loss))
-                        writer.add_scalar('Loss', loss.item(), i + epoch*len(self.dataloader.dataset))
+                        writer.add_scalar('Loss', loss.item(), i + epoch * len(self.dataloader))
             hrt.save_model(checkpoint_number=epoch)
 
     def save_model(self, save_dir=os.path.join(MODELS_DIR, "hr_checkpoints"), checkpoint_number=None):
